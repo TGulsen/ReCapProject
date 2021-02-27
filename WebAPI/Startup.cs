56984934ac3a061b1105ff9024954695f8661caf
Tesnim Gülşen <kuzen_teso646@hotmail.com>
2021-02-27 13:18:04 +0300

    with Autofac

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,20 +30,20 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
+            // services.AddSingleton<ICarService, CarManager>();
+            // services.AddSingleton<ICarDal, EfCarDal>();
 
-            services.AddSingleton<IBrandService, BrandManager>();
-            services.AddSingleton<IBrandDal, EfBrandDal>();
+            // services.AddSingleton<IBrandService, BrandManager>();
+           // services.AddSingleton<IBrandDal, EfBrandDal>();
 
-            services.AddSingleton<IColorService, ColorManager>();
-            services.AddSingleton<IColorDal, EfColorDal>();
+            // services.AddSingleton<IColorService, ColorManager>();
+            // services.AddSingleton<IColorDal, EfColorDal>();
 
-            services.AddSingleton<ICustomerService, CustomerManager>();
-            services.AddSingleton<ICustomerDal, EfCustomerDal>();
+           // services.AddSingleton<ICustomerService, CustomerManager>();
+            // services.AddSingleton<ICustomerDal, EfCustomerDal>();
 
-            services.AddSingleton<IRentalService, RentalManager>();
-            services.AddSingleton<IRentalDal, EfRentalDal>();
+            // services.AddSingleton<IRentalService, RentalManager>();
+         //   services.AddSingleton<IRentalDal, EfRentalDal>();
 
 
         }
