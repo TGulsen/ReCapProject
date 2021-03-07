a7f870f2352063bb500ae3a9f940dae1369e5043
Tesnim <43925549+TGulsen@users.noreply.github.com>
2021-03-07 19:04:05 +0300

    Add files via upload

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        // Doğrulama kurallarının olduğu class, doğrulanacak nesne
+        public static void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
